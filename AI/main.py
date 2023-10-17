0b9c2d6c53bdd6516d785a8b3b80ec7b5bf23f0d
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-10-17 15:41:56 -0500

    Have all the code for the training ready, but my computer isn't powerfull enough

--- AI/main.py
+++ AI/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from ultralytics import YOLO
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Load Model
+    model = YOLO("yolov8n.yaml") # build a new model from scratch
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Use the model
+    results = model.train(data="config.yaml", epochs = 1) # train the model
