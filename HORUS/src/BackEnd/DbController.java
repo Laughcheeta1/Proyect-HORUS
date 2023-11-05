9cb15872458d2ba0801afaf81bed8ef28e63fad8
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-11-05 17:57:14 -0500

    Started again the intelliJ proyect for the app

---
+++ HORUS/src/BackEnd/DbController.java
@@ -0,0 +1,11 @@
+package BackEnd;
+
+public class DbController {
+    private final String dbConnection = "";
+
+    public Plane getPlaneByName(String name)
+    {
+        // TODO: make this method
+        return null;
+    }
+}
