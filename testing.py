d91a5bf98889f951c5fa46aaf1b0bd5712b306b0
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-11-09 23:35:21 -0500

    This files are not needed for testing

--- testing.py
+++
@@ -1,6 +0,0 @@
-import ultralytics
-
-model = ultralytics.YOLO("C:\\Users\\Usuario\\Documents\\Universidad\\Requisitos y modelos\\CODE\\previousTraining.pt")
-results = model("C:\\Users\\Usuario\\Documents\\Universidad\\Requisitos y modelos\CODE\\videosTest\\planes.png", show = True)
-
-print(results[0].boxes.cpu().numpy().cls)
