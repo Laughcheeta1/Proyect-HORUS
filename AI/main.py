ff587e1c9b0d13f190bde3ab07a0b6e152131683
DiegoColBdz <dcollazosbermudez@gmail.com>
2023-11-10 09:25:09 -0500

    Se sube el ultimo modelo definitivo

--- AI/main.py
+++ AI/main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load Model
-    model = YOLO("C:\\Users\\Asus\\Desktop\\ProyectoHorus\\runs\\detect\\train8\\weights\\best.pt") # build a new model from scratch
+    model = YOLO("C:\\Users\\Asus\\Desktop\\ProyectoHorus\\modeloAnterior.pt") # build a new model from scratch
 
     # Use the model
-    results = model.train(data="AI\config.yaml", epochs = 200) # train the model
+    results = model.train(data="AI\config.yaml", epochs =100) # train the model
--- AI/testing.py
+++ AI/testing.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("C:\\Users\\Asus\\Desktop\\ProyectoHorus\\runs\\detect\\train8\\weights\\best.pt")
+model = YOLO("C:\\Users\\Asus\\Desktop\\ProyectoHorus\\runs\\detect\\train15\\weights\\best.pt")
 
 model.predict("C:\\Users\\Asus\\Desktop\\ProyectoHorus\\videosTest\\y2mate.com - 10 Minutes of FIGHTER JETS in Action_480p.mp4", save = True, show = True, conf = 0.7)
