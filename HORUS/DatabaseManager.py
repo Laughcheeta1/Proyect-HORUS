18494eea6f6e690124c90d6e3870e908cea17250
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-11-10 01:41:51 -0500

    Turns out I had copied the collection name wrong

--- HORUS/DatabaseManager.py
+++ HORUS/DatabaseManager.py
@@ -2,11 +2,9 @@ from pymongo import MongoClient
 
 class DataBaseManager:
     def __init__(self):
-        self.planes = MongoClient("localhost", 27017).horus.planes # Directly get the planes collection in the DB
+        self.planes = MongoClient("localhost", 27017).horus.Planes # Directly get the planes collection in the DB
 
     # Input: the class number of the desired plane
     # Returns: the Json stored in the database of that plane
     def getPlaneByClassId(self, classNumber):
-        plane = self.planes.find_one({"classNumber" : classNumber})
-        print(classNumber)
-        return plane
+        return self.planes.find_one({"classNumber" : classNumber})
